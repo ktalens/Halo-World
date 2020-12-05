f4e640c5f8589d0973453fe9b95f77aa15817d5b
ktalens <ktal66@yahoo.com>
2020-12-05 15:00:45 -0800

    more small ejs & css style changes

--- controllers/profile.js
+++ controllers/profile.js
@@ -20,36 +20,46 @@ router.get('/', isLoggedIn, (req, res)=>{
         let h5short= h5response.data.Results[0].Result.CustomStats
         axios.get(`${haloMCCUrl}${player}`)
         .then(mccResponse=>{
-            //res.send(mccResponse.data)
-    
-            res.render('profile/profile',{
-                halo5: {
-                    gamertag: h5response.data.Results[0].Id,
-                    TotalKills: h5short.TotalKills,
-                    TotalDeaths:h5short.TotalDeaths,
-                    TotalAssists:h5short.TotalAssists,
-                    TotalGamesCompleted: h5short.TotalGamesCompleted,
-                    TotalGamesWon:h5short.TotalGamesWon,
-                    TotalGamesLost: h5short.TotalGamesLost,
-                    TotalGamesTied: h5short.TotalGamesTied,
-                    TotalHeadshots: h5short.TotalHeadshots,
-                    TotalShotsFired: h5short.TotalShotsFired,
-                    TotalShotsLanded: h5short.TotalShotsLanded,
-                    TotalMeleeKills: h5short.TotalMeleeKills,
-                    TotalAssassinations: h5short.TotalAssassinations,
-                    TotalGrenadeKills:h5short.TotalGrenadeKills,
-                    TotalPowerWeaponKills: h5short.TotalPowerWeaponKills,
-                    WeaponWithMostKills: h5short.WeaponWithMostKills,
-                    WeaponStats: h5short.WeaponStats
-                },
-                haloMCC: mccResponse.data 
+            let userId= res.locals.currentUser.id
+            db.user.findOne({
+                where: {id: userId},
+                include: [db.weapon, db.map,db.strategy]
+            })
+            .then(userData=>{
+                //res.send(userData)
+                res.render('profile/profile',{
+                    halo5: {
+                        gamertag: h5response.data.Results[0].Id,
+                        TotalKills: h5short.TotalKills,
+                        TotalDeaths:h5short.TotalDeaths,
+                        TotalAssists:h5short.TotalAssists,
+                        TotalGamesCompleted: h5short.TotalGamesCompleted,
+                        TotalGamesWon:h5short.TotalGamesWon,
+                        TotalGamesLost: h5short.TotalGamesLost,
+                        TotalGamesTied: h5short.TotalGamesTied,
+                        TotalHeadshots: h5short.TotalHeadshots,
+                        TotalShotsFired: h5short.TotalShotsFired,
+                        TotalShotsLanded: h5short.TotalShotsLanded,
+                        TotalMeleeKills: h5short.TotalMeleeKills,
+                        TotalAssassinations: h5short.TotalAssassinations,
+                        TotalGrenadeKills:h5short.TotalGrenadeKills,
+                        TotalPowerWeaponKills: h5short.TotalPowerWeaponKills,
+                        WeaponWithMostKills: h5short.WeaponWithMostKills,
+                        WeaponStats: h5short.WeaponStats
+                    },
+                    haloMCC: mccResponse.data,
+                    userData: userData 
+                })
+            })
+            .catch(err=>{
+                req.flash('error', err.message)
+                res.redirect('/profile/profile')
             })
         })
         .catch(err=>{
             req.flash('error', err.message)
             res.redirect('/profile/profile')
         })
-        //res.send(h5response.data)
     })
     .catch(err=>{
         req.flash('error', err.message)
